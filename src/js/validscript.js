68d35a7f7fe15967b8216da69606783af6f0a3b6
manislawa <malwina.witoszek82@gmail.com>
2019-12-06 11:49:35 +0100

    Remove ignored files

--- src/js/validscript.js
+++ src/js/validscript.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     // the first item is active after loading
     $('#name').focus();
-    $('#all').on('click', function(){
+    $('#all').on('click', function () {
         var checkboxes = $('#form').find('.check');
         if (this.checked) {
             checkboxes.attr('checked', 'true');
@@ -21,25 +21,34 @@ $(document).ready(function () {
                 required: true,
                 email: true
             },
+            message: {
+                required: true,
+                minlength: 5
+            }
         },
 
         messages: {
             name: "* Podanie imienia jest wymagane.",
             email: "* Podanie adresu e-mail jest wymagane.",
+            message: {
+                required: "* Uzupełnij wiadomość.",
+                minlength: "Wiadomość powinna zawierać minimum 5 znakow"}
         },
-        showErrors: function(errorMap, errorList) {
-        this.defaultShowErrors();
+        showErrors: function (errorMap, errorList) {
+            this.defaultShowErrors();
         }
     });
 
-        //  the message after confirming the form
-        var form = $("#form");
-        form.valid();
-        $("#send").click(function(e) {
-            e.preventDefault();
-            if(form.valid() === true) {
-               alert( "Formularz został przesłany");}
-            else{
-                alert( "Formularz NIE został przesłany, uzupełnij pola wymagane");}
-            });
+    //  the message after confirming the form
+    var form = $("#form");
+    form.valid();
+    $("#send").click(function (e) {
+        e.preventDefault();
+        if (form.valid() === true) {
+            alert("Formularz został przesłany");
+        }
+        else {
+            alert("Formularz NIE został przesłany, uzupełnij pola wymagane");
+        }
     });
+});
